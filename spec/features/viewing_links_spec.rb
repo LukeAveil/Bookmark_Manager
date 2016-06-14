0cba3bdb1e3fff49e1178b5ed4ca7e95aac76bf5
Luke <lukeaveil@yahoo.com>
2016-06-14 09:35:40 +0100

    initial

--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-feature 'Viewing links' do
+feature 'Viewing links.' do
   scenario 'I can see existing links on the links page' do
   	Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
   	visit '/links'
